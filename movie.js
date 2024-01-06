fb261c9567e3f5e79c1fd959dfc0c0523d66bcbf
VitaliyV22 <vitanskyyv@gmail.com>
2024-01-05 20:00:20 -0600

    working on part 2

--- movie.js
+++ movie.js
@@ -1,8 +1,14 @@
 const movieValue = $("#Title").val();
 const ratingValue = $("#Rating").val();
+var button = $("<button>").text("Delete");
+var button2 = $("<button>").text("Delete");
 
 $("#form").on("submit", function (event) {
-  $("#titleList").append("<li>movieValue</li>");
-  $("#ratingList").append("<li>ratingValue</li>");
+  $("#titleList").append("<li>movieValue</li>",button);
+  $("#ratingList").append("<li>ratingValue</li>", button2);
   event.preventDefault();
 });
+
+$(button).on( "click", function() {
+    mov
+  })
